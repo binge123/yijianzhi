9969354dff1ac0a33a54e1fc419d9b47d35f8c5a
binge123 <912349941@qq.com>
2015-11-18 08:11:48 +0800

    no message

---
+++ YiJianZhi/app/src/main/java/com/best/demo/yijianzhi/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.best.demo.yijianzhi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/11/17.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
