5ef207ffaf0a09da1f5107673b1adf510f689772
binge123 <912349941@qq.com>
2015-11-18 10:21:20 +0800

    no message

--- YiJianZhi/app/src/main/java/com/best/demo/yijianzhi/LoginActivity.java
+++ YiJianZhi/app/src/main/java/com/best/demo/yijianzhi/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.best.demo.yijianzhi;
 
-<<<<<<< HEAD
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -12,10 +11,4 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-=======
-/**
- * Created by wen on 2015/11/17.
- */
-public class LoginActivity {
->>>>>>> develop
 }
