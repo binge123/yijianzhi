32aae7e8c8c50150732a89cb255fc587099aeea8
Timermis <1217475329@qq.com>
2015-11-23 13:45:52 +0800

    no message

--- YiJianZhi/app/src/main/java/com/best/demo/yijianzhi/ZhuCeActivity.java
+++ YiJianZhi/app/src/main/java/com/best/demo/yijianzhi/ZhuCeActivity.java
@@ -11,8 +11,5 @@ public class ZhuCeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zhuce);
-
-
-        
     }
 }
