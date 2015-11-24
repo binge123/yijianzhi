3a7d56f9ecb43233e5365954181583a087f5c405
taotutu <1171813790@qq.com>
2015-11-24 08:13:16 +0800

    no message

--- YiJianZhi/app/src/main/java/com/best/bean/MyListViewForScollProbiem.java
+++ YiJianZhi/app/src/main/java/com/best/bean/MyListViewForScollProbiem.java
@@ -7,22 +7,19 @@ import android.widget.ListView;
 /**
  * Created by TaoHongTu on 2015/11/20.
  */
-public class MyListViewForScollProbiem extends ListView {
+public class MyListViewForScollProbiem extends ListView{
+
     public MyListViewForScollProbiem(Context context){
         super(context);
-    }
 
+    }
     public MyListViewForScollProbiem(Context context,AttributeSet attributeSet){
         super(context,attributeSet);
 
     }
     public MyListViewForScollProbiem(Context context,AttributeSet attributeSet, int defStyleAttr){
-        super(context,attributeSet,defStyleAttr);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int endHeight =MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
-        super.onMeasure(widthMeasureSpec, endHeight);
+        super(context, attributeSet, defStyleAttr);
     }
 }
+
+
